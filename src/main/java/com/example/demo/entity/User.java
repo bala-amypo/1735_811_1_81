3b133b346b58615ea654afd663f41aeacde85391
bala-amypo <bala@amypo.in>
2025-12-18 06:22:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,49 +4,78 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
-public class User{
-
-     @Id
-     @GeneratedValue(strategy=GenerationType.IDENTITY)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String fullname;
+    private String fullName;
     private String email;
     private String department;
     private String role;
     private String password;
-    private LocalDateTime:createdAt;
+    private LocalDateTime createdAt;
+    public User(Long id, String fullName, String email, String department, String role, String password, LocalDateTime createdAt) {
+        this.fullName = fullName;
+        this.email = email;
+        this.department = department;
+        this.role = role;
+        this.password = password;
+        this.createdAt = createdAt;
+    }
 
+    public User() {
+    }
 
-    public User(Long id,String fullname,String email,String department,
-    String role,String password, LocalDateTime createdAt){
-        this.fullname=fullname;
-        this.email=email;
-        this.department=department;
-        this.role=role;
-        this.password
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
-    public StudentEntity(){
 
+    public String getFullName() {
+        return this.fullName;
     }
-    public void setName(String name){
-        this.name=name;
+
+    public void setEmail(String email) {
+        this.email = email;
     }
-    public String getName(){
-        return this.name;
-    }
-    public void setEmail(){
-        this.email=email;
-    }
-    public String getEmail(){
+
+    public String getEmail() {
         return this.email;
     }
-    public void setCgpa(){
-        this.cgpa=cgpa;
+
+    public void setDepartment(String department) {
+        this.department = department;
     }
-    public float getcgpa(){
-        return this.cgpa;   
+
+    public String getDepartment() {
+        return this.department;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
     }
 }
+
 
