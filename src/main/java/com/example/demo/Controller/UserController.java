f76af9d9b1df69bed5453e6cc2546bb6cb570a2c
bala-amypo <bala@amypo.in>
2025-12-19 08:28:45 +0000

    User commit

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -1,39 +1,31 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 public class UserController {
 
-    private UserService ser;
+    private final UserService service;
 
-    public UserController(UserService ser) {
-        this.ser = ser;
+    public UserController(UserService service) {
+        this.service = service;
     }
-
-    @PostMapping("/register")
-    public User registerUser(@RequestBody User user) {
-        return ser.registerUser(user);
+    @PostMapping
+    public User createUser(@Valid @RequestBody User user) {
+        return service.createUser(user);
     }
-
     @GetMapping
     public List<User> getAllUsers() {
-        return ser.getAllUsers();
+        return service.getAllUsers();
     }
-
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
-        return ser.getUserById(id);
+        return service.getUserById(id);
     }
 }
