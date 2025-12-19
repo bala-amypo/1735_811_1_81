5b917883de1296d1aa566fa6a0adcbe847804c9e
bala-amypo <bala@amypo.in>
2025-12-19 08:22:45 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -7,28 +7,21 @@ import java.time.LocalDateTime;
 
 @Entity
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String fullName;
-
     @Column(unique = true)
     private String email;
-
     @NotBlank(message = "Department is required")
     private String department;
-
     private String role;
-
     @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
-
     private LocalDateTime createdAt;
+    public User() {
 
-    public User() {}
-
+    }
     public User(Long id, String fullName, String email, String department,
                 String role, String password, LocalDateTime createdAt) {
         this.id = id;
@@ -58,23 +51,44 @@ public class User {
     }
 
     public String getFullName() { 
-            return fullName; 
+           return fullName; 
     }
     public void setFullName(String fullName) { 
-            this.fullName = fullName; }
+            this.fullName = fullName; 
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { 
+           return email; 
+    }
+    public void setEmail(String email) { 
+           this.email = email; 
+    }
 
-    public String getDepartment() { return department; }
-    public void setDepartment(String department) { this.department = department; }
+    public String getDepartment() { 
+            return department; 
+    }
+    public void setDepartment(String department) { 
+            this.department = department; 
+    }
 
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
+    public String getRole() { 
+            return role;
+    }
+    public void setRole(String role) {
+            this.role = role; 
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getPassword() {
+            return password; 
+    }
+    public void setPassword(String password) {
+            this.password = password; 
+    }
 
-    public LocalDateTime getCreatedAt() { return createdAt; }
-    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+    public LocalDateTime getCreatedAt() { 
+            return createdAt;
+     }
+    public void setCreatedAt(LocalDateTime createdAt) { 
+            this.createdAt = createdAt; 
+    }
 }
