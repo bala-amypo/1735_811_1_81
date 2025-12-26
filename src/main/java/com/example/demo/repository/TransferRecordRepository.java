3c6a8a3fde4d7085655c9a7b4673f42ff02deb7e
bala-amypo <bala@amypo.in>
2025-12-26 08:44:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/TransferRecordRepository.java
+++ src/main/java/com/example/demo/repository/TransferRecordRepository.java
@@ -2,10 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.TransferRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface TransferRecordRepository extends JpaRepository<TransferRecord, Long> {
-
-    List<TransferRecord> findByAssetId(Long assetId);
+    // Matches test cases t43 and t81: findByAsset_Id
+    List<TransferRecord> findByAsset_Id(Long assetId);
 }
